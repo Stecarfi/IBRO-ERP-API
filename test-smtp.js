367f63097e8cf22ce82a71c72923b4a56f318db8
Stephanie Carrasquilla <admin@ibro.com>
2026-07-23 22:58:27 -0500

    RESEND

---
+++ test-smtp.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "lidmarkpub@gmail.com",
+    pass: "hxgi rfzd zydc alpm",
+  },
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP SUCCESS: Server is ready to take our messages");
+  }
+});
